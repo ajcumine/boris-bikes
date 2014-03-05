e0088cd4bcd3e102e93e75cea4438ce98643a767
Andrew <aj.cumine@btinternet.com>
2014-03-05 20:39:13 +0000

    added garage

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,10 +3,6 @@ require "./lib/docking_station"
 describe DockingStation do
 
   let(:station) { DockingStation.new(:capacity => 123) }
-
-  def fill_station(station)
-    20.times {station.dock(Bike.new)}
-  end
   
   it "should allow setting default capacity on initialising" do
     expect(station.capacity).to eq(123)
