70d39a564a3384b4f514cb894f92bc419263db6b
Andrew <aj.cumine@btinternet.com>
2014-03-06 19:47:43 +0000

    van collects from garage

--- lib/van.rb
+++ lib/van.rb
@@ -2,9 +2,16 @@ class Van
 
   include BikeContainer
 
-  def collect_available_bikes_from(station)
-    station.available_bikes.each do |bike|
+  def collect_broken_bikes_from_station(station)
+    station.bikes.reject {|bike| !bike.broken?}.each do |bike|
       station.release(bike)
+      self.dock(bike)
+    end
+  end
+
+  def collect_working_bikes_from_garage(garage)
+    garage.bikes.reject {|bike| bike.broken?}.each do |bike|
+      garage.release(bike)
       self.dock(bike)
     end
   end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -2,18 +2,25 @@ require "./lib/bike_container"
 require "./lib/van"
 require "./lib/docking_station"
 require "./lib/bike"
+require "./lib/garage"
 
 describe Van do
   
-  it "should collect available bikes fromt the station" do
-    station = DockingStation.new
-    van = Van.new
-    available_bike = Bike.new
-    station.dock(available_bike)
-    van.collect_available_bikes_from(station)
-    expect(van.bikes).to include(available_bike)
+  it "should collect broken bikes from the station" do
+    station, van, broken_bike = DockingStation.new, Van.new, Bike.new
+    broken_bike.break
+    station.dock(broken_bike)
+    van.collect_broken_bikes_from_station(station)
+    expect(van.bikes).to include(broken_bike)
     expect(station).to be_empty
   end
 
+  it "should collect working bikes from the garage" do
+    garage, van, working_bike = Garage.new, Van.new, Bike.new
+    garage.dock(working_bike)
+    van.collect_working_bikes_from_garage(garage)
+    expect(van.bikes).to include(working_bike)
+    expect(garage).to be_empty
+  end
 
 end
